eb5cd59351748fded65069c0360c3df4bc3a5e6b
tomer0702 <utkarsh.tomer@successive.tech>
2021-01-15 18:48:33 +0530

    add : mutation feature/58433

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,11 @@
 import pkg from 'merge-graphql-schemas';
-import path from 'path';
-import * as user from './user/index.js';
-
-const __dirname= path.resolve();
-
 const { fileLoader, mergeTypes } = pkg;
+import path from 'path';
+// eslint-disable-next-line import/extensions
+import * as user from './user/index.js';
+import * as trainee from './trainee/index.js';
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.resolve();
 
 const typesArray = fileLoader(path.join(__dirname, './**/*.graphql'));
 
@@ -13,7 +14,11 @@ const typeDefs = mergeTypes(typesArray, { all: true });
 export default {
   resolvers: {
     Query: {
-      ...user.Query
+      ...user.Query,
+      ...trainee.Query
+    },
+    Mutation: {
+      ...trainee.Mutation
     }
   },
   typeDefs
